000396eb16f5d80dd8c2725c815cc699f41985a2
rosie.stevenson <rosie.stevenson@tubemogul.com>
2017-11-18 20:58:45 -0800

    adding tokens

--- app.js
+++ app.js
@@ -11,22 +11,20 @@ const
   schedule = require('node-schedule'),
   request = require('request'),
   reminders = [],
-  APP_SECRET = config.appSecret,
-  VALIDATION_TOKEN = config.validationToken,
-  PAGE_ACCESS_TOKEN = config.pageAccessToken,
   IMG_BASE_PATH = 'https://rodnolan.github.io/posterific-static-images/',
   EventEmitter = require('events').EventEmitter;
 var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
 
 const bot = new BootBot({
   accessToken: process.env.ACCESS_TOKEN,
   verifyToken: process.env.VERIFY_TOKEN,
   appSecret: process.env.APP_SECRET
 })
+
+
+app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 bot.setGreetingText("Hello, my name is Celeste. What would you like to be called?");
 bot.hear('hello', (payload, chat) => {
