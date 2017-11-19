e887c8fcf22c349e74409b82d39af7c0c9afc98f
rosie.stevenson <rosie.stevenson@tubemogul.com>
2017-11-18 21:07:20 -0800

    fixing validation

--- app.js
+++ app.js
@@ -14,17 +14,42 @@ const
   IMG_BASE_PATH = 'https://rodnolan.github.io/posterific-static-images/',
   EventEmitter = require('events').EventEmitter;
 var app = express();
+app.set('port', config.port);
+app.set('view engine', 'ejs');
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
+app.use(express.static('public'));
 
-const bot = new BootBot({
-  accessToken:  config.pageAccessToken,
-  verifyToken: config.validationToken,
-  appSecret: config.appSecret
+// App Dashboard > Dashboard > click the Show button in the App Secret field
+const APP_SECRET = config.appSecret;
+
+// App Dashboard > Webhooks > Edit Subscription > copy whatever random value you decide to use in the Verify Token field
+const VALIDATION_TOKEN = config.validationToken;
+
+// App Dashboard > Messenger > Settings > Token Generation > select your page > copy the token that appears
+const PAGE_ACCESS_TOKEN = config.pageAccessToken;
+
+
+app.get('/', function(req, res) {
+  res.send("hey there boi")
+})
+
+app.get('/webhook/', function(req, res) {
+  if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN){
+    return res.send(req.query['hub.challenge'])
+  }
+  res.send('wrong token')
+})
+
+app.listen(app.get('port'), function(){
+  console.log('Started on port', app.get('port'))
 })
 
 
-app.set('port', (process.env.PORT || 5000));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+const bot = new BootBot({
+  accessToken: PAGE_ACCESS_TOKEN,
+  verifyToken: VALIDATION_TOKEN,
+  appSecret: APP_SECRET
+})
 
 bot.setGreetingText("Hello, my name is Celeste. What would you like to be called?");
 bot.hear('hello', (payload, chat) => {
